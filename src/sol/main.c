9e9eb7de3c34432f665d6a22715b1c8707c3f3f0
YaroslavPr17 <yarikpristalov1702@yandex.ru>
2023-10-30 16:33:43 +0300

    Add: Multi-thread Acceleration-matrix-impl

--- src/sol/main.c
+++ src/sol/main.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <pthread.h>
+#include <float.h>
 
 #define DT 0.05
+#define EPS 10e-64
 
 typedef struct
 {
@@ -13,6 +16,10 @@ int bodies, timeSteps;
 double *masses, GravConstant;
 vector **acc_table;
 vector *positions, *velocities, *accelerations;
+
+int thread_count = 0;
+
+
 
 vector addVectors(vector a, vector b)
 {
@@ -42,7 +49,7 @@ double mod(vector a)
 
 void initiateSystem(char *fileName)
 {
-    int i;
+    long i;
     FILE *fp = fopen(fileName, "r");
 
     fscanf(fp, "%lf%d%d", &GravConstant, &bodies, &timeSteps);
@@ -55,6 +62,9 @@ void initiateSystem(char *fileName)
     acc_table = (vector**)malloc(bodies * sizeof(vector*));
     for (i = 0; i < bodies; ++i){
         acc_table[i] = (vector*)malloc(bodies * sizeof(vector));
+        for (long j = 0; j < bodies; ++j){
+            acc_table[i][j].x = DBL_MAX;            
+        }
     }
 
     for (i = 0; i < bodies; i++)
@@ -69,7 +79,7 @@ void initiateSystem(char *fileName)
 
 void resolveCollisions()
 {
-    int i, j;
+    long i, j;
 
     for (i = 0; i < bodies - 1; i++)
         for (j = i + 1; j < bodies; j++)
@@ -83,49 +93,121 @@ void resolveCollisions()
         }
 }
 
-void computeAccelerations()
-{
-    int i, j;
+// void computeAccelerations()
+// {
+//     long i, j;
 
-    for (i = 0; i < bodies; i++)
+//     for (i = 0; i < bodies; i++)
+//     {
+//         for (j = 0; j < bodies; j++)
+//         {
+//             if (i < j)   // Верхняя часть 
+//             {
+//                 acc_table[i][j] =
+//                     scaleVector(
+//                         GravConstant * masses[j] / pow(
+//                             mod(subtractVectors(positions[i], positions[j])), 
+//                             3), 
+//                         subtractVectors(positions[j], positions[i]));
+//             }
+//             else if(i > j){
+//                 acc_table[i][j] = scaleVector(-1, acc_table[j][i]);
+//             }
+//         }
+//     }
+
+//     for (i = 0; i < bodies; ++i)
+//     {
+//         accelerations[i].x = 0;
+//         accelerations[i].y = 0;
+
+//         for (j = 0; j < bodies; ++j)
+//         {
+//             if (i != j)
+//                 accelerations[i] = addVectors(accelerations[i], acc_table[i][j]);
+//         }
+//     }
+
+//     // for (i = 0; i < bodies; i++)
+//     // {
+//     //     printf("(%15.13lf, %15.13lf)\t", accelerations[i].x, accelerations[i].y);
+//     // }
+//     // printf("\n");
+// }
+
+
+
+void* computeAccelerationsRoutine(void* rank){
+    long thread_n = (long) rank;
+    long batch_size = bodies / thread_count;
+    long start = batch_size * thread_n;
+    long finish = start + batch_size;
+
+    printf("thread_n %ld\n", thread_n);
+    printf("bodies %d\n", bodies);
+    printf("thread_count %d\n", thread_count);
+    printf("batch_size %ld\n", batch_size);
+    printf("start %ld\n", start);
+    printf("finish %ld\n", finish);
+
+    
+
+    long i, j;
+
+    for (i = start; i < finish; i++)
     {
         for (j = 0; j < bodies; j++)
         {
-            if (i < j)   // Верхняя часть 
-            {
-                acc_table[i][j] =
-                    scaleVector(
-                        GravConstant * masses[j] / pow(
-                            mod(subtractVectors(positions[i], positions[j])), 
-                            3), 
-                        subtractVectors(positions[j], positions[i]));
+            if (i != j){
+                if (fabs(acc_table[j][i].x - DBL_MAX) < EPS)   // Верхняя часть 
+                {
+                    acc_table[i][j] =
+                        scaleVector(
+                            GravConstant * masses[j] / pow(
+                                mod(subtractVectors(positions[i], positions[j])), 
+                                3), 
+                            subtractVectors(positions[j], positions[i]));
+                }
+                else {
+                    acc_table[i][j] = scaleVector(-1, acc_table[j][i]);
+                }
             }
-            else if(i > j){
-                acc_table[i][j] = scaleVector(-1, acc_table[j][i]);
-            }
+
         }
-
-
     }
 
-    for (i = 0; i < bodies; ++i)
+    return NULL;
+}
+
+
+void computeAccelerations(){
+    pthread_t* thread_handles = NULL;
+    thread_handles = malloc(thread_count * sizeof(pthread_t));
+    
+    for (long i = 0; i < thread_count; ++i ) {
+        pthread_create(&thread_handles[i], NULL, computeAccelerationsRoutine, (void*) i);
+    }
+    for (long i = 0; i < thread_count; ++i) {
+        pthread_join(thread_handles[i], NULL);
+    }
+    free(thread_handles);
+
+    for (long i = 0; i < bodies; ++i)
     {
         accelerations[i].x = 0;
         accelerations[i].y = 0;
 
-        for (j = 0; j < bodies; ++j)
+        for (long j = 0; j < bodies; ++j)
         {
-            // if (i != j)
+            if (i != j)
                 accelerations[i] = addVectors(accelerations[i], acc_table[i][j]);
         }
     }
 
-    // for (i = 0; i < bodies; i++)
-    // {
-    //     printf("(%15.13lf, %15.13lf)\t", accelerations[i].x, accelerations[i].y);
-    // }
-    // printf("\n");
 }
+
+
+
 
 void computeVelocities()
 {
@@ -158,14 +240,15 @@ int main(int argC, char *argV[])
 {
     int i, j;
 
-    if (argC != 3)
+    if (argC != 4)
         printf("Usage : %s <file name containing system configuration data>", argV[0]);
     else
     {
 
-        FILE *fp = fopen(argV[2], "w");
+        FILE *fp = fopen(argV[3], "w");
+        thread_count = strtol(argV[1], NULL, 10);
+        initiateSystem(argV[2]);
 
-        initiateSystem(argV[1]);
         fprintf(fp, "Body;x;y;vx;;vy\n");
         for (i = 0; i < timeSteps; i++)
         {
@@ -182,16 +265,5 @@ int main(int argC, char *argV[])
         }
     }
 
-
-
-
-    // for (i = 0; i < bodies; i++)
-    // {
-    //     for (j = 0; j < bodies; j++)
-    //     {
-    //         printf("(%15.13lf, %15.13lf)", acc_table[i][j].x, acc_table[i][j].y);
-    //     }
-    //     printf("\n");
-    // }
     return 0;
 }
