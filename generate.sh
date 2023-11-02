9c4ef1d84ad17967a96e2d80aeb001fec741e7de
YaroslavPr17 <yarikpristalov1702@yandex.ru>
2023-11-02 13:18:09 +0300

    feat: CLI params for inputs generation

--- generate.sh
+++ generate.sh
@@ -1,9 +1,10 @@
 # !/bin/bash
 
-START_VALUE=100
-FINISH_VALUE=2000
+START_VALUE=$1
+FINISH_VALUE=$2
+STEP=$3
 
-for ((n_bodies=$START_VALUE; n_bodies <= $FINISH_VALUE; n_bodies += 100))
+for ((n_bodies=$START_VALUE; n_bodies < $FINISH_VALUE; n_bodies += $STEP))
 do
     ./out/gen_run $n_bodies 1000
     sleep 1
