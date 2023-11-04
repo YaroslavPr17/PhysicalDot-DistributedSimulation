2b0bccc469d5881d2e5012df67124fca632f1f23
YaroslavPr17 <yarikpristalov1702@yandex.ru>
2023-11-04 12:12:43 +0300

    refactor: Fix testing environment & Tests conducted

--- src/test_unit.c
+++ src/test_unit.c
@@ -4,7 +4,7 @@
 #include "multi_thread.h"
 
 #define MAX_SINGLE_TEST_COUNT 300
-#define MAX_RETRIES_COUNT 100
+#define MAX_RETRIES_COUNT 20
 
 
 int main(int argc, char** argv) {
@@ -23,7 +23,7 @@ int main(int argc, char** argv) {
     n_bodies = strtol(argv[2], NULL, 10);
     HT = argv[3];
     src = argv[4];
-    
+
     if (argc == 6){
         n_retries = strtol(argv[5], NULL, 10);
     }
@@ -39,30 +39,34 @@ int main(int argc, char** argv) {
     sprintf(fout, file_out, n_bodies);
     sprintf(fmetr, metrics_filename, src);
 
-    printf("---> Input file: %s\n", fin);
-    printf("---> Output file: %s\n", fout);
+    // printf("---> Input file: %s\n", fin);
+    // printf("---> Output file: %s\n", fout);
 
-    printf("---> Metrics file: %s\n", fmetr);
+    // printf("---> Metrics file: %s\n", fmetr);
     FILE *metr_file = fopen(fmetr, "a");
 
     double sum_exec_time = 0;
 
     if (argc == 5){
-        printf("---> Retries count is not provided and will be automatically adjusted.\n");
+        // printf("---> Retries count is not provided and will be automatically adjusted.\n");
 
         double temp_time = make_single_run(n_threads, fin, fout);
         sum_exec_time += temp_time;
         n_retries = MAX_SINGLE_TEST_COUNT / temp_time;
         if (n_retries > MAX_RETRIES_COUNT) 
             n_retries = MAX_RETRIES_COUNT;
+        if (n_retries < 1) 
+            n_retries = 2;
     }
 
-    printf("---> Testing %ld bodies on %d threads for %ld retries.\n", n_bodies, n_threads, n_retries);
+    // printf("---> Testing %ld bodies on %d threads for %ld retries.\n", n_bodies, n_threads, n_retries);
+
+    printf("\nLOGS: n_threads=%d, n_bodies=%ld, n_retries=%ld\n", n_threads, n_bodies, n_retries);
 
 
     printf("Retries ");
     for (long i = 0; i < n_retries; ++i){
-        if (i % 10 == 0){
+        if (i < 10 || i % 10 == 0){
             printf("%ld ", i + 1);
             fflush(stdout);
         }
@@ -72,8 +76,11 @@ int main(int argc, char** argv) {
 
     if (argc == 5)
         n_retries++;
+
+    printf("Result=%lf\n", sum_exec_time / n_retries);
+
     
-    printf("---> Mean execution time over %ld retries = %4.12lf s.\n", n_retries, sum_exec_time / n_retries);
+    // printf("---> Mean execution time over %ld retries = %4.12lf s.\n", n_retries, sum_exec_time / n_retries);
     fprintf(metr_file, "%ld,%d,%ld,%4.12lf,%s\n", n_bodies, n_threads, n_retries, sum_exec_time / n_retries, HT);
 
     fclose(metr_file);
--- test.sh
+++ test.sh
@@ -4,8 +4,15 @@ START_VALUE=$1
 FINISH_VALUE=$2
 STEP=$3
 
+SOURCE_NAME=$4
+IS_HT=$5
+        
+gcc src/$SOURCE_NAME.c -lm src/test_unit.c -o ./out/test_unit_run
+
 for ((n_bodies=$START_VALUE; n_bodies < $FINISH_VALUE; n_bodies += $STEP))
 do
-    ./out/gen_run $n_bodies 1000
-    sleep 1
+    for n_threads in 1 2 4 6 12 24 30 40 60 120
+    do
+        ./out/test_unit_run $n_threads $n_bodies $IS_HT $SOURCE_NAME
+    done
 done
